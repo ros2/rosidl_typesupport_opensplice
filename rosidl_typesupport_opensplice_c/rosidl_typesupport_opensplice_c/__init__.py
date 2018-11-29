50f627480b7ba2ee042adbcb0cc1f9f979c89879
Jacob Perron <jacob@openrobotics.org>
2018-11-29 09:41:57 -0800

    Update C typesupport EmPy files

--- rosidl_typesupport_opensplice_c/rosidl_typesupport_opensplice_c/__init__.py
+++ rosidl_typesupport_opensplice_c/rosidl_typesupport_opensplice_c/__init__.py
@@ -20,7 +20,7 @@ from rosidl_cmake import generate_files
 def generate_typesupport_opensplice_c(arguments_file):
     mapping = {
        'idl__rosidl_typesupport_opensplice_c.h.em': '%s__rosidl_typesupport_opensplice_c.h',
-       'idl__dds_opensplice__type_support.c.em': 'dds_opensplice/%s__type_support.c',
+       'idl__dds_opensplice__type_support.c.em': 'dds_opensplice_c/%s__type_support_c.cpp',
     }
 
     generate_files(arguments_file, mapping)
